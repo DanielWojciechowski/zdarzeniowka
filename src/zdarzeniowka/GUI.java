1feb0ec1bd9753268286a87a574cfbef3084c711
Acquen <acierniewska@gmail.com>
2014-01-03 15:48:38 +0100

    klasa MenuListModel

--- src/zdarzeniowka/GUI.java
+++ src/zdarzeniowka/GUI.java
@@ -1,13 +1,44 @@
 package zdarzeniowka;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
-	JFrame frame;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JList;
+
+public class GUI implements ActionListener{
+	private JFrame frame;
+	private JLayeredPane layeredPane;
+	private JButton menuButton;
+	private MenuListModel menuListModel;
+	private JList<String> menuList;
+	private boolean menuVisibility;
+	
 
 	public void addComponents(Container pane){
+		pane.setLayout(new GridBagLayout());
+		layeredPane = new JLayeredPane();
+		layeredPane.setLayout(new GridLayout(2,3));
+		GridBagConstraints c = new GridBagConstraints();
+		
+		menuButton = new JButton("Menu");
+		menuButton.addActionListener(this);
+		layeredPane.add(menuButton, JLayeredPane.PALETTE_LAYER);
+		menuListModel = new MenuListModel();
+		menuList = new JList<>(menuListModel);
+		menuList.setVisible(false);
+		layeredPane.add(menuList, JLayeredPane.POPUP_LAYER);
+		
+		
+	
+		pane.add(layeredPane, c);
 		
 	}
 	
@@ -15,6 +46,7 @@ public class GUI {
 		frame = new JFrame("System ewidencyjny sieci komputerowej w DS - AC&DW");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setMinimumSize(new Dimension(950, 700));
+		
 		addComponents(frame);
 		
 		frame.pack();
@@ -22,6 +54,15 @@ public class GUI {
 		frame.setVisible(true);
 		
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if (source == menuButton){
+			menuVisibility = !menuVisibility;
+			menuList.setVisible(menuVisibility);
+		}	
 	}
 	
 
---
+++ src/zdarzeniowka/MenuListModel.java
@@ -0,0 +1,14 @@
+package zdarzeniowka;
+
+import javax.swing.DefaultListModel;
+
+public class MenuListModel extends DefaultListModel<String>{
+	private static final long serialVersionUID = -3256759296340102196L;
+
+	public MenuListModel(){
+		super();
+		addElement("D");
+		addElement("B");
+	}
+
+}
