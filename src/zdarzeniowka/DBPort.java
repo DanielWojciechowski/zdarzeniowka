b9499f87c363f1a70628382c6dcd24140b117497
Daniel <Daniel@DanielAsus>
2014-01-04 16:14:19 +0100

    zrobione klasy do bazy danych

---
+++ src/zdarzeniowka/DBPort.java
@@ -0,0 +1,42 @@
+package zdarzeniowka;
+
+public class DBPort {
+	private int portNo;
+	private int idHistory;
+	private int idUser;
+	private int idDevice;
+	private double dataUse;
+	
+	public int getPortNo() {
+		return portNo;
+	}
+	public void setPortNo(int portNo) {
+		this.portNo = portNo;
+	}
+	public int getIdHistory() {
+		return idHistory;
+	}
+	public void setIdHistory(int idHistory) {
+		this.idHistory = idHistory;
+	}
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public int getIdDevice() {
+		return idDevice;
+	}
+	public void setIdDevice(int idDevice) {
+		this.idDevice = idDevice;
+	}
+	public double getDataUse() {
+		return dataUse;
+	}
+	public void setDataUse(double dataUse) {
+		this.dataUse = dataUse;
+	}
+	
+	
+}
---
+++ src/zdarzeniowka/DBRoom.java
@@ -0,0 +1,15 @@
+package zdarzeniowka;
+
+public class DBRoom {
+	private int roomNo;
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+	
+
+}
